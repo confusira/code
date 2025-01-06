1fe44b12a043472b9af5a5b2105f2d4542facbe7
confusira <14787341+confusira@user.noreply.gitee.com>
2025-01-06 17:59:15 +0800

    1.6

---
+++ sort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+void bubblesort(int n,int* num){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(num[i]<num[j]) swap(num[i],num[j]);
+        }
+    }
+}
+
+int main(){
+
+    int a[10]={1,4,5,3,9,7,2,8,10,6};
+
+    bubblesort(10,a);
+
+    for(int i=0;i<10;i++) cout << a[i] << endl;
+
+    return 0;
+}
