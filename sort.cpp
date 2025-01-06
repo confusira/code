86b327758e25bab5f89bc4df47c25d153cf1ecfb
confusira <14787341+confusira@user.noreply.gitee.com>
2025-01-06 22:36:00 +0800

    1.6.2

--- sort.cpp
+++ sort.cpp
@@ -10,11 +10,21 @@ void bubblesort(int n,int* num){
     }
 }
 
+void selectionsort(int n,int *num){
+    for(int i=0;i<n;i++){
+        int min=i;
+        for(int j=i+1;j<n;j++){
+            if(num[j]<num[min]) min=j;
+        }
+        swap(num[min],num[i]);
+    }
+}
+
 int main(){
 
     int a[10]={1,4,5,3,9,7,2,8,10,6};
 
-    bubblesort(10,a);
+    selectionsort(10,a);
 
     for(int i=0;i<10;i++) cout << a[i] << endl;
 
